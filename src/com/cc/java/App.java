0ff8ebce07d59fbe74ca27c3bfd64104d2d1675a
DonCerebro1 <85444482+DonCerebro1@users.noreply.github.com>
2021-12-01 15:09:27 +0100

    added new Class

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -1,8 +1,12 @@
 package com.cc.java;
 
-public class App {
+public class App 
+{
     
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        Cat cat = new Cat();
+        System.out.println(cat);
 
     
     }
