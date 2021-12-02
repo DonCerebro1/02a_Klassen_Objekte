b5ba368e29c7858c2bba9caf1b126d78b21bb36c
DonCerebro1 <85444482+DonCerebro1@users.noreply.github.com>
2021-12-02 12:33:29 +0100

    added Constructer and Getter/Setter (encapsulation) and limited access to getName with if

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -1,15 +1,28 @@
 package com.cc.java;
 
-public class App {
+public class App  {
 
     
     public static void main(String[] args){
     
-        Cat cat = new Cat();
-        System.out.println("Blick von aussen: " + cat);
-        cat.tellYourAdress();
+        Cat cat = new Cat("Grizabella", 23, "Rot");                                // Klasse --> Objekt / Instanz
+        
+        // Objekt mit call der Methode
+        // cat.getInfo("Grizabella", 23, "Rot");
+        
+        // set den Namen von Cat zu "Chris"
+        // cat.setName("chris");                                                  // aktuell kann der name nicht geändert werden. Weil zugriff in getName mit dem If eingeschränkt ist
 
-    
+        // gibt den Namen von Cat aus
+        // System.out.println(cat.getName());                                     // cat.getName() --> deshalb weil wir in der Class Cat die Variablen auf Privat gesetzt haben
+
+        System.out.println("-------------------------");
+
+        Cat cat2 = new Cat("Alonzo", 29, "Schwarz");                               // Klasse --> Objekt / Instanz
+
+        // Objekt mit call der Methode
+        // cat2.getInfo("Alonzo", 29, "Schwarz");
+        
     }
 
 }
--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -2,10 +2,51 @@ package com.cc.java;
 
 public class Cat{
 
-    public void tellYourAdress() {
-        System.out.println("Blick von innen: " + this );
+    //attribute oder Feld nennt man Variablen in einer Klasse
+    private String name;
+    private int age;
+    private String furColor;
+
+    //Konstrukter (eigener Konstrukter)
+    public Cat(String name, int age, String furColor){
+        getInfo(name, age, furColor);
 
     }
+
+    // Methode
+    public void getInfo(String name, int age, String furColor){
+        this.name   = name;
+        this.age    = age;
+        this.furColor   = furColor;
+        System.out.println("Mein name ist: " + name + " und bin " + age + " alt" + " mit der Fellfarbe " + furColor);
+    }
+
+    //Getter and Setter für alle Variablen (Kapselung)
+    public String getName() {
+        return name;
+    }
     
+    //mit dem IF kann ich den Namen nicht ändern der bleibt der gleiche wie im Konstrukter. So kann man den Zugriff von dritten Beschränken
+    public void setName(String name) {
+        if(false){
+        this.name = name;
+        
+    }}
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFurColor() {
+        return furColor;
+    }
+
+    public void setFurColor(String furColor) {
+        this.furColor = furColor;
+    }
     
 }
